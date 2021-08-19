fe2e5a9408e95968d55606bea451b1c37803e54e
Tomas Gil <tomasgil1234@gmail.com>
2021-08-19 19:42:43 -0300

    feat: add login and dashboard screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// Screens
+import Login from 'screens/login'
+import Dashboard from 'screens/dashboard'
+// en el proceso de login el usuario obtiene un token y un refresh token
+
+// con axios interceptors agregamos el token a las requests
+
+// si alguna request devuelve 401, usamos el refresh token
+// para pedir un nuevo token
+// que pasa con la request que se hizo y fallo? tenemos que volver a hacerla?
+
+// si el refresh token falla o no existe
+// redirigimos al usuario a la pagina de login
+
+// podemos implementar tambien un sistema que se anticipe a la expiracion
+// del token y lo refresque en ciertos momentos
 
 const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
