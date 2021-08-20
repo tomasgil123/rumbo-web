76ca3c058f264f2382dc722c7c2308c3dc8f1180
Tomas Gil <tomasgil1234@gmail.com>
2021-08-20 00:02:47 -0300

    refactor: delete commetns

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // Screens
 import Login from 'screens/login'
 import Dashboard from 'screens/dashboard'
-// en el proceso de login el usuario obtiene un token y un refresh token
-
-// con axios interceptors agregamos el token a las requests
-
-// si alguna request devuelve 401, usamos el refresh token
-// para pedir un nuevo token
-// que pasa con la request que se hizo y fallo? tenemos que volver a hacerla?
-
-// si el refresh token falla o no existe
-// redirigimos al usuario a la pagina de login
-
-// podemos implementar tambien un sistema que se anticipe a la expiracion
-// del token y lo refresque en ciertos momentos
 
 const App = (): JSX.Element => {
   return (
