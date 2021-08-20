0a56438776ac754b448cfd29ec044f8da6390a9c
Tomas Gil <tomasgil1234@gmail.com>
2021-08-20 00:23:52 -0300

    feat: add react hook form to login screen

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const LoginScreen = (): JSX.Element => {
-  return <div>Login</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ shouldUseNativeValidation: false })
+  const onSubmit = (data: any): void => console.log(data)
+
+  return (
+    <div>
+      Login
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('email', { required: true })} />
+          {errors.email && <span>This field is required</span>}
+          <input {...register('password', { required: true })} />
+          {errors.password && <span>This field is required</span>}
+          <input type="submit" value="Ingresar" />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default LoginScreen
