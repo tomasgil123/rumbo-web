0e06a742c9d4dfe0547dc38cfb89d2ab4b3da45e
Tomas Gil <tomasgil1234@gmail.com>
2021-08-20 07:46:11 -0300

    feat: hide dashboard route behind login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 // Screens
 import Login from 'screens/login'
 import Dashboard from 'screens/dashboard'
+// Utils
+import { getLocalAccessToken } from 'utils/session'
 
 const App = (): JSX.Element => {
+  const token = getLocalAccessToken()
   return (
     <Router>
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
+          <Route path="/dashboard">{token ? <Dashboard /> : <Redirect to="/" />}</Route>
           <Route path="/">
             <Login />
           </Route>
