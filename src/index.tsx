7430daae626d903743eca184aacaba0d16fe374a
Tomas Gil <tomasgil1234@gmail.com>
2021-08-21 17:14:02 -0300

    feat: add simple line icons

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import 'simple-line-icons/css/simple-line-icons.css'
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser')
--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 // components
 import Layout from 'components/layout'
+
 const DashboardScreen = (): JSX.Element => {
-  return <div>Dashboard</div>
+  return (
+    <div className="max-w-screen-sm mt-8 md:mt-16 mx-auto px-4">
+      <div className="rounded shadow-lg p-4">
+        <div>Tareas plan operativo</div>
+        <div>
+          <i className="icon-note"></i>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Layout(DashboardScreen)
