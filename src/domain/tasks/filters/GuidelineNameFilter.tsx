ba811369510f8a5084fb59aa1a3cadb9b0575821
javier martinicorena <martinicorenaj@gmail.com>
2021-09-23 15:36:22 -0300

    fix:add focus on icons && reduce guidelineFilter input

--- src/domain/tasks/filters/GuidelineNameFilter.tsx
+++ src/domain/tasks/filters/GuidelineNameFilter.tsx
@@ -7,10 +7,10 @@ interface props {
 
 const GuidelineNameFilter = ({ handleSearchChange, guidelineNameFilter }: props): JSX.Element => {
   return (
-    <div>
-      ingresar lineamiento
+    <div className="flex flex-col items-center justify-center max-w-screen-sm mt-8 md:mt-16 mx-auto px-4">
+      Ingresar Lineamiento
       <input
-        className="focus:text-white text-white mb-4 w-full outline-none bg-primary-dark border-b-2 border-white"
+        className="focus:text-white text-white mb-4 outline-none bg-primary-dark border-b-2 border-black "
         onChange={handleSearchChange}
         value={guidelineNameFilter}
       />
--- src/domain/tasks/filters/statusFilter.tsx
+++ src/domain/tasks/filters/statusFilter.tsx
@@ -10,37 +10,37 @@ interface Props {
 const StatusFilter = ({ taskByStatus, handleClick }: Props): JSX.Element => {
   return (
     <div className="flex flex-row justify-center ">
-      <button onClick={(): void => handleClick(TaskStatus.new)}>
-        <div className="p-2 md:p-4">
-          <div className="flex items-center justify-around">
-            <i className="icon-note text-disabled text-sm md:text-2xl" />
+      <button className="group" onClick={(): void => handleClick(TaskStatus.new)}>
+        <div className="p-2 md:p-4 group-focus:bg-disabled-light">
+          <div className="flex items-center justify-around ">
+            <i className="icon-note group-focus:text-base  text-danger-light text-sm md:text-2xl" />
             <span>{taskByStatus[TaskStatus.new].length}</span>
           </div>
           <p>{TaskStatus.new}</p>
         </div>
       </button>
-      <button onClick={(): void => handleClick(TaskStatus.expired)}>
-        <div className="p-2 md:p-4">
-          <div className="flex items-center justify-around">
-            <i className="icon-fire text-disabled text-sm md:text-2xl"></i>
+      <button className="group" onClick={(): void => handleClick(TaskStatus.expired)}>
+        <div className="p-2 md:p-4 group-focus:bg-disabled-light">
+          <div className="flex items-center justify-around ">
+            <i className="icon-fire group-focus:text-base  text-danger text-sm md:text-2xl"></i>
             {taskByStatus[TaskStatus.expired].length}
           </div>
           <p>{TaskStatus.expired}</p>
         </div>
       </button>
-      <button onClick={(): void => handleClick(TaskStatus.pending)}>
-        <div className="p-2 md:p-4">
-          <div className="flex items-center justify-around">
-            <i className="icon-note text-disabled text-sm md:text-2xl"></i>
+      <button className="group" onClick={(): void => handleClick(TaskStatus.pending)}>
+        <div className="p-2 md:p-4 group-focus:bg-disabled-light">
+          <div className="flex items-center justify-around ">
+            <i className="icon-note group-focus:text-base  text-primary-light text-sm md:text-2xl"></i>
             {taskByStatus[TaskStatus.pending].length}
           </div>
           <p>{TaskStatus.pending}</p>
         </div>
       </button>
-      <button onClick={(): void => handleClick(TaskStatus.done)}>
-        <div className="p-2 md:p-4">
-          <div className="flex items-center justify-around">
-            <i className="icon-note text-disabled text-sm md:text-2xl"></i>
+      <button className="group" onClick={(): void => handleClick(TaskStatus.done)}>
+        <div className="p-2 md:p-4 group-focus:bg-disabled-light">
+          <div className="flex items-center justify-around ">
+            <i className="icon-note group-focus:text-base  text-success text-sm md:text-2xl"></i>
             {taskByStatus[TaskStatus.done].length}
           </div>
           <p>{TaskStatus.done}</p>
