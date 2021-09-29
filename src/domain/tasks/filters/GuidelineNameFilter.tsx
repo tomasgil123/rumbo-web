ac09a081abd9b8b7a37400e8cd98746a9c6d87b5
javier martinicorena <martinicorenaj@gmail.com>
2021-09-29 19:08:57 -0300

    fix: change guidelineNameFilter input

--- src/domain/tasks/filters/GuidelineNameFilter.tsx
+++ src/domain/tasks/filters/GuidelineNameFilter.tsx
@@ -8,11 +8,12 @@ interface props {
 const GuidelineNameFilter = ({ handleSearchChange, guidelineNameFilter }: props): JSX.Element => {
   return (
     <div className="flex flex-col items-center justify-center max-w-screen-sm mt-8 md:mt-16 mx-auto px-4">
-      Ingresar Lineamiento
       <input
-        className="focus:text-white text-white mb-4 outline-none bg-primary-dark border-b-2 border-black "
+        type="search"
+        className="text-center focus:text-disabled text-disabled mb-4 outline-none  border-b-2 border-black"
         onChange={handleSearchChange}
         value={guidelineNameFilter}
+        placeholder="Ingresar Lineamiento..."
       />
     </div>
   )
