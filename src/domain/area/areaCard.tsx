f870464b7ab67bbca0450d27aa7e41264464ec06
javier martinicorena <martinicorenaj@gmail.com>
2021-10-05 20:43:30 -0300

    fix:rebase conflict 2

--- src/domain/area/areaCard.tsx
+++ src/domain/area/areaCard.tsx
@@ -3,9 +3,10 @@ import ProgressCircle from 'components/progressCircle'
 //types
 import { SurveyActive } from 'types/survey'
 import { AuditProgram } from 'types/auditProgram'
+import { Area } from 'types/area'
 
 interface props {
-  area: any
+  area: Area
   survey: SurveyActive
   auditProgram: AuditProgram
 }
@@ -17,34 +18,33 @@ const AreaCard = ({ area, survey, auditProgram }: props): JSX.Element => {
   )
 
   return (
-    <div className="flex flex-row rounded shadow-lg p-4 mt-4 bg-white">
-      <p>{area.name}</p>
-
-      <div className="flex-1 flex flex-col items-center md:ml-16">
-        <div className="flex-grow pb-2 md:pb-4 md:text-lg text-gray-700">Puntaje total</div>
-        <ProgressCircle
-          radius={45}
-          progresses={[
-            { percent: 100, color: isAreaApproved ? 'text-success-light' : 'text-danger-light' },
-            { percent: areaPoints, color: isAreaApproved ? 'text-success' : 'text-danger' },
-          ]}
-          value={areaPoints}
-          isPercentage={false}
-        />
-      </div>
-      <div className="flex-1 flex flex-col items-center md:mr-16">
-        <div className="text-center pb-2 md:pb-4 md:text-lg text-gray-700">
-          Porcentaje de aprobación
+    <div className="flex flex-col rounded shadow-lg p-4 mt-4 bg-white">
+      <p style={{ color: area.color }} className="text-center">
+        {area.name}
+      </p>
+      <div className="flex flex-row justify-center rounded ">
+        <div className="flex-1 flex flex-col items-center md:ml-16">
+          <ProgressCircle
+            radius={45}
+            progresses={[
+              { percent: 100, color: isAreaApproved ? 'text-success-light' : 'text-danger-light' },
+              { percent: areaPoints, color: isAreaApproved ? 'text-success' : 'text-danger' },
+            ]}
+            value={areaPoints}
+            isPercentage={false}
+          />
         </div>
-        <ProgressCircle
-          radius={45}
-          progresses={[
-            { percent: 100, color: isAreaApproved ? 'text-success-light' : 'text-danger-light' },
-            { percent: areaPercentage, color: isAreaApproved ? 'text-success' : 'text-danger' },
-          ]}
-          value={areaPercentage}
-          isPercentage={true}
-        />
+        <div className="flex-1 flex flex-col items-center md:mr-16">
+          <ProgressCircle
+            radius={45}
+            progresses={[
+              { percent: 100, color: isAreaApproved ? 'text-success-light' : 'text-danger-light' },
+              { percent: areaPercentage, color: isAreaApproved ? 'text-success' : 'text-danger' },
+            ]}
+            value={areaPercentage}
+            isPercentage={true}
+          />
+        </div>
       </div>
     </div>
   )
