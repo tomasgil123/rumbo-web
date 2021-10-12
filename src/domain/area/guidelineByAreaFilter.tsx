6a4ab0f11a6d1dc5041e27ce77d3f49b28442ec1
javier martinicorena <martinicorenaj@gmail.com>
2021-10-12 18:15:32 -0300

    fix:align icon & delete imput type search

--- src/domain/area/guidelineByAreaFilter.tsx
+++ src/domain/area/guidelineByAreaFilter.tsx
@@ -6,15 +6,14 @@ interface props {
 }
 const GuidelineByAreaFilter = ({ handleSearchChange, guidelineNameByArea }: props): JSX.Element => {
   return (
-    <div className="flex flex-row justify-center w-full mt-1 ">
+    <div className="flex flex-row justify-center w-full mt-1  ">
       <input
-        type="search"
         className="text-center focus:text-disabled text-disabled mb-4 outline-none  border-b-2 border-black"
         onChange={handleSearchChange}
         value={guidelineNameByArea}
         placeholder="Buscar Lineamiento"
       />
-      <i className="icon-magnifier text-sm md:text-lg" />
+      <i className=" icon-magnifier mt-2 text-sm md:text-lg" />
     </div>
   )
 }
